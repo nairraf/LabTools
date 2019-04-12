fb988f0b11f9aecaa92719b537ee37a03a3c449d
Ian Farr <ian.farr@farrworks.com>
2019-04-12 00:24:22 -0400

    YAML PoC Complete
    YAML is clean, and the C3 parser is great. This is a working example for YAML

--- Populate AD/DataConfigFileTests/Program.cs
+++ Populate AD/DataConfigFileTests/Program.cs
@@ -1,56 +1,80 @@
 ﻿using Nett;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace DataConfigFileTests
 {
     class Program
     {
-        public class Configuration
+        public class Settings
         {
-            public Company Company { get; set; }
-            public Dictionary<string, Dictionary<string, Locations>> Locations { get; set;}
-            //public Dictionary<string, Dictionary<string, OrgTop>> OrgTop { get; set; }
+            public string CompanyName { get; set; }
+            public List<Location> Locations { get; set; }
+            public List<Employee> Org { get; set; }
         }
 
-        public class Company
+        public class Location
         {
             public string Name { get; set; }
-        }
-
-        public class Locations
-        {
-            public string Name { get; set; }
-            public string rContinent { get; set; }
-            public string rCountry { get; set; }
-            public string rPopulation { get; set; }
+            public string RContinent { get; set; }
+            public string RPopulation { get; set; }
             public string Country { get; set; }
             public string City { get; set; }
         }
 
-        public class OrgTop
+        public class Employee
         {
-            public string Department { get; set; }
             public string Title { get; set; }
+            public string Department { get; set; }
             public string Location { get; set; }
             public string NumPeople { get; set; }
-            public string Name { get; set; }
+            public List<Employee> Employees { get; set; }
         }
 
         static void Main(string[] args)
         {
-            var data = Toml.ReadFile("OrgChart.toml").ToDictionary();
-            var confCompany = (Dictionary<string, object>)data["Company"];
-            var confLocation = data["Locations"];
-            var org = data["OrgTop"];
- 
-            Console.WriteLine("Company Name: {0})", confCompany["Name"]);
-            //Console.WriteLine("Company Name: " + ;
+            //YAML Desirialize to Object Example
+            var deserializer = new DeserializerBuilder().WithNamingConvention(new CamelCaseNamingConvention()).Build();
+            var settings = deserializer.Deserialize<Settings>(File.OpenText("Settings.yaml"));
+
+            Console.WriteLine("The Second Location Name: {0}", settings.Locations[1].Name);
+            Console.WriteLine("Company Name: {0}", settings.CompanyName);
+            Console.WriteLine("Org:");
+            PrintEmployees(settings.Org);
 
             Console.ReadLine();
         }
+
+        public static void PrintEmployees(List<Employee> Employees, Employee Parent = null, int Level = 0)
+        {
+            foreach (Employee emp in Employees)
+            {
+                if (Parent != null)
+                {
+                    for (int i = 0; i < Level; i++)
+                    {
+                        Console.Write("    ");
+                    }
+                    Console.WriteLine("Title: {0}, Parent: {1}", emp.Title, Parent.Title);
+                } else
+                {
+                    Console.WriteLine("Title: {0}", emp.Title);
+                }
+                
+                if (emp.Employees != null && emp.Employees.Any())
+                {
+                    Level += 1;
+                    PrintEmployees(emp.Employees, emp, Level);
+                }
+            }
+        }
+
     }
 }
